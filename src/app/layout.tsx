ce490fc4e6c026c8c6f97f12d955541bdd3081b7
Luis Alejandro Gomez Angeles <gomezangelesluisalejandro@gmail.com>
2025-09-03 00:33:15 -0600

    Agregar dependencias de Vercel Analytics y Speed Insights, e integrarlas en el layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +22,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/Expanda_Isotipo.png",         // clásico
     shortcut: "/Expanda_Isotipo.png",
-    apple: "/Expanda_Isotipo.png",     
+    apple: "/Expanda_Isotipo.png",
   },
   keywords: [
     "ERP México",
@@ -72,6 +74,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
